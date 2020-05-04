f164a8b408c2d9c70a45d9092b020f4982d16994
AluskaM <31692205+AluskaM@users.noreply.github.com>
2020-05-04 16:53:10 +0300

    Add files via upload

---
+++ export.py
@@ -0,0 +1,29 @@
+import csv
+import cx_Oracle
+username = 'bd'
+password = 'makarenko'
+database = 'localhost/xe'
+connection = cx_Oracle.connect(username,password, database)
+cursor = connection.cursor()
+
+csv_file = open('Google-Playstore-32K.csv', 'w', newline='')
+
+
+tables = ['Category', 'Audience', 'App', 'Reviews']
+
+
+try:
+    for table in tables:
+       with open(table + '.csv', 'w', newline = '') as csvfile:
+       cursor.execute('SELECT * FROM ' + table)
+       cursor.execute(query)
+       row = cursor.fetchone()
+                 
+       csv_writer = csv.writer(csv_file, delimiter=',')
+       csv_writer.writerow(['App_name', 'Category_name', 'Audience_type', 'Reviews', 'Price', 'Data'])
+        for data in row:
+            csv_writer.writerow(data)
+
+cursor.close()
+connection.close()
+csv_file.close()
