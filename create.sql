547c7d3b37dbd82e6226f49cec11bfc6a57e7847
AluskaM <31692205+AluskaM@users.noreply.github.com>
2020-05-06 17:14:32 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -9,24 +9,28 @@ ALTER TABLE Audience
     ADD CONSTRAINT  audience_type_pk PRIMARY KEY( audience_type);
     
 CREATE TABLE App(
+    id INTEGER NOT NULL,
     app_name VARCHAR2(50) NOT NULL,
     category_name VARCHAR2(20) NOT NULL,
     audience_type VARCHAR2(20) NOT NULL,
     price NUMBER NOT NULL);
 ALTER TABLE App
-    ADD CONSTRAINT app_name_pk PRIMARY KEY(app_name); 
+    ADD CONSTRAINT id_pk PRIMARY KEY(id); 
 
 CREATE TABLE Reviews(
+    id integer NOT NULL,
     review_date date,
     app_name VARCHAR2(50) NOT NULL,
     reviews_count INT NOT NULL);
 ALTER TABLE Reviews
-    ADD CONSTRAINT  app_names_pk PRIMARY KEY(app_name);
+    ADD CONSTRAINT  idd_pk PRIMARY KEY(id);
     
 ALTER TABLE App
-    ADD CONSTRAINT app_name_fk FOREIGN KEY(app_name) REFERENCES App(app_name); 
+    ADD CONSTRAINT id_fk FOREIGN KEY(id) REFERENCES App(id); 
 ALTER TABLE App
     ADD CONSTRAINT category_name_fk FOREIGN KEY(category_name) REFERENCES Category(category_name);
 ALTER TABLE App
     ADD CONSTRAINT audience_type_fk FOREIGN KEY(audience_type) REFERENCES Audience(audience_type);
+    
+    
     
