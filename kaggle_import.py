7e63af622b69dbf14a36a538bed2e63507ef7977
AluskaM <31692205+AluskaM@users.noreply.github.com>
2020-05-08 16:18:58 +0300

    Update kaggle_import.py

--- kaggle_import.py
+++ kaggle_import.py
@@ -1,5 +1,3 @@
-
-
 import csv
 import cx_Oracle
 username = 'bd'
@@ -20,46 +18,51 @@ for table in tables:
     cursor.execute("DELETE FROM " + table)
 row_num = 0
 i=1
+try:
+    for row in reader:
+        app_name = row[0]
+        category_name = row[1]
+        reviews_count = row[3]
+        price = row[6]
+        new_price = str(price)
+        audience_type = row[7]
 
-for row in reader:
-    app_name = row[0]
-    category_name = row[1]
-    reviews_count = row[3]
-    price = row[6]
-    new_price = str(price)
-    audience_type = row[7]
+        if category_name not in category_unique:
+                category_unique.append(category_name)
+                query = '''INSERT INTO Category(category_name) VALUES(:category_name)'''
+                cursor.execute(query, category_name=category_name)
 
-    if category_name not in category_unique:
-        category_unique.append(category_name)
-        query = '''INSERT INTO Category(category_name) VALUES(:category_name)'''
-        cursor.execute(query, category_name=category_name)
+        if audience_type not in audience_unique:
+            audience_unique.append(audience_type)
+            query = '''INSERT INTO Audience(audience_type) VALUES(:audience_type)'''
+            cursor.execute(query, audience_type=audience_type)
+    
+        if reviews_count == '':
+            reviews_count = 0
+        query = '''
+                     INSERT INTO Reviews(id, reviews_count, app_name) 
+                         VALUES(:id, :reviews_count, :app_name)'''
+       app_name = app_name.encode('utf-8', 'replace').decode('utf-8', 'ignore')
+        cursor.execute(query, id=i, reviews_count=reviews_count, app_name=app_name)
 
-    if audience_type not in audience_unique:
-        audience_unique.append(audience_type)
-        query = '''INSERT INTO Audience(audience_type) VALUES(:audience_type)'''
-        cursor.execute(query, audience_type=audience_type)
+        if new_price[0] == '$':
+            f_price = float(new_price[1:])
+        else:
+            f_price = 0
+        query = '''
+               INSERT INTO App(id, app_name, category_name, audience_type, price) 
+                   VALUES(:id, :app_name, :category_name, :audience_type, :price)'''
+        app_name.encode('utf-8', 'replace').decode('utf-8', 'ignore')
+        cursor.execute(query, id=i, app_name=app_name, category_name=category_name, audience_type=audience_type, price=f_price)
+        row_num += 1
+        i+=1
 
-    if reviews_count == '':
-        reviews_count = 0
-    query = '''
-                 INSERT INTO Reviews(id, reviews_count, app_name) 
-                     VALUES(:id, :reviews_count, :app_name)'''
-   app_name = app_name.encode('utf-8', 'replace').decode('utf-8', 'ignore')
-    cursor.execute(query, id=i, reviews_count=reviews_count, app_name=app_name)
+except:
+    print('Error',i)
+    raise
 
-    if new_price[0] == '$':
-        f_price = float(new_price[1:])
-    else:
-        f_price = 0
-    query = '''
-           INSERT INTO App(id, app_name, category_name, audience_type, price) 
-               VALUES(:id, :app_name, :category_name, :audience_type, :price)'''
-    app_name.encode('utf-8', 'replace').decode('utf-8', 'ignore')
-    cursor.execute(query, id=i, app_name=app_name, category_name=category_name, audience_type=audience_type, price=f_price)
-    row_num += 1
-    i+=1
-
-connection.commit()
-cursor.close()
-connection.close()
-csv_file.close()
+finally:
+    connection.commit()
+    cursor.close()
+    connection.close()
+    csv_file.close()
