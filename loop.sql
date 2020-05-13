6c4053b7b268abb3502948bfd41a0f6539ccd8b6
AluskaM <31692205+AluskaM@users.noreply.github.com>
2020-05-13 11:21:55 +0300

    Update loop.sql

--- loop.sql
+++ loop.sql
@@ -1,22 +1,26 @@
 DECLARE
+    var_id              reviews.id%TYPE;
     var_app_name        reviews.app_name%TYPE := 'app_name';
     var_reviews_count   reviews.reviews_count%TYPE;
-    var_review_date     reviews.review_date%TYPE;
+
     
 BEGIN
 
-    var_review_date := TO_DATE( 'October 01, 2020', 'MONTH DD, YYYY' );
+    
     FOR i IN 1..10 LOOP
     INSERT INTO reviews (
+        id,
         app_name,
-        reviews_count,
-        review_date)
+        reviews_count
+      )
         VALUES (
+        i,
         TRIM(var_app_name)||'_' || i,
-        i * 500,
-        var_review_date
+        i * 500
     );
 
     END LOOP;
 
 END;
+
+select * from Reviews
